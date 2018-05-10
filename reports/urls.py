639ca1177e64e05e415649c9ac024cf22c911a2d
Fernando Barra <fernandobarra2207@gmail.com>
2018-05-10 14:20:08 -0600

    Sets the necessary settings for static file deployment in production

--- reports/urls.py
+++ reports/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('report_log/', views.AllReports.as_view(), name='report_log'),
     path('submit_status/', views.submit_status, name='submit_status')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
